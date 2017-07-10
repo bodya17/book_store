72202b3d8153736481b03548635f668a14595caa
dreadnough <odin_ogame@ukr.net>
2017-07-10 18:40:33 +0300

    Update bookRoutes.js

--- routes/bookRoutes.js
+++ routes/bookRoutes.js
@@ -1,14 +1,14 @@
-const express = require('express');
+const express = require('express'); // 1 time const is enough
 const router = express.Router();
-
+// no blank lines should be here
 const bookController = require('../controllers/bookController'); 
 
-router.get('', function(req, res, next) {
+router.get('', function(req, res, next) { // next - unused
     res.render('new-book');
 });
 
-router.post('/', bookController.createBook);
+router.post('/', bookController.createBook); // diff signature lines 6 and 10  , "create" is enough
 
-router.get('/allbooks', bookController.listBooks);
+router.get('/allbooks', bookController.listBooks);  // "/books" - means all books , listBooks - "list" is enough
 
 module.exports = router;
